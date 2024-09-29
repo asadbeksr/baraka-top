625c97acec0bb9ececfaa033a30bf1be11c199ea
asadbek <asadbeksr@gmail.com>
2024-09-30 02:52:16 +0500

    ui mode system

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <SessionProvider>
           <ThemeProvider
             attribute="class"
-            defaultTheme="light"
+            defaultTheme="system"
             enableSystem
             disableTransitionOnChange
           >
